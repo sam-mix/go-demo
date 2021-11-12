6e46627b0bdde92b2ee878fdb15134ec98df3ddf
米鸿坤 <1045262335@qq.com>
2021-11-12 21:39:40 +0800

    chero: tidy

--- float-demo/main.go
+++ float-demo/main.go
@@ -13,7 +13,7 @@ func main() {
 	bits := math.Float32bits(number)
 
 	binary := fmt.Sprintf("%.32b", bits)
-	fmt.Printf("Bit Pattern: %s | %s %s | %s %s %s %s %s %s",
+	fmt.Printf("Bit Pattern: %s | %s %s | %s %s %s %s %s %s\n",
 		binary[0:1],
 		binary[1:5],
 		binary[5:9],
@@ -25,4 +25,28 @@ func main() {
 		binary[28:32],
 	)
 
+	fmt.Println(binary)
+
+	bias := 127
+	sign := bits & (1 << 31)
+	exponentRaw := int(bits >> 23)
+	exponent := exponentRaw - bias
+	var mantissa float64
+	for i, bit := range binary[9:32] {
+		if bit == 49 {
+			position := i + 1
+			bitValue := math.Pow(2, float64(position))
+			fractional := 1 / bitValue
+			mantissa += fractional
+		}
+	}
+	value := (1 + mantissa) * math.Pow(2, float64(exponent))
+
+	fmt.Printf("Sign : %d, Exponent : %d (%d), Mantissa : %f, Value : %f\n",
+		sign,
+		exponentRaw,
+		exponent,
+		mantissa,
+		value,
+	)
 }
